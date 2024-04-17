fb4953d4cb31e63b1813e915a025cf8636e85e88
Jay-r <jhayar.zuniga004@gmail.com>
2024-04-17 18:13:28 +0800

    Create the Profile View

--- backend/customer/views.py
+++ backend/customer/views.py
@@ -3,7 +3,8 @@ from django.conf import settings
 from django.core.mail import EmailMultiAlternatives
 from django.template.loader import render_to_string
 
-from userauths.models import User
+from userauths.models import User, Profile
+from userauths.serializer import ProfileSerializer
 
 from store.models import Product, Category, Cart, Tax, CartOrder, CartOrderItem, Coupon, Notification, Review
 from store.serializers import ProductSerializer, CategorySerializer, CartSerializer, CartOrderSerializer, CartOrderItemSerializer, CartOrder, CartOrderItem, CouponSerializer, Coupon, NotificationSerializer, ReviewSerializer
@@ -39,3 +40,14 @@ class OrderDetailAPIView(generics.RetrieveAPIView):
 
         order = CartOrder.objects.get(buyer=user, oid=order_oid, payment_status='paid')
         return order
+    
+class ProfileView(generics.RetrieveAPIView):
+    serializer_class = ProfileSerializer
+    permission_classes = [AllowAny]
+
+    def get_object(self):
+        user_id = self.kwargs['user_id']
+
+        user = User.objects.get(id=user_id)
+        profile = Profile.objects.get(user=user)
+        return profile
